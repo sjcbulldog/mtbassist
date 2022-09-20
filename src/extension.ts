045382efc1ebeb96066c14eb4a52f7c024223e50
Butch Griffin <butchg@comcast.net>
2022-09-20 07:08:54 -0700

    updated to add startup page

--- src/extension.ts
+++ src/extension.ts
@@ -6,7 +6,7 @@ import { MTBAssistDocumentProvider } from './mtbdoc';
 import { mtbCreateProject, mtbImportProject, mtbRunEditor, mtbShowDoc } from './mtbcommands';
 import path = require('path');
 import fs = require('fs') ;
-import { getMTBDocumentationTreeProvider, getMTBProgramsTreeProvider, initMtbInfo } from './mtbinfo';
+import { getModusToolboxChannel, getMTBDocumentationTreeProvider, getMTBProgramsTreeProvider, initMtbInfo } from './mtbinfo';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -54,6 +54,53 @@ export function activate(context: vscode.ExtensionContext) {
 			}
 		}) ;
 	}
+
+	let panel: vscode.WebviewPanel = vscode.window.createWebviewPanel(
+		'mtbassist', 
+		'ModusToolbox', 
+		vscode.ViewColumn.One, 
+        {
+			enableScripts: true
+		  }
+	) ;
+	let html: string = "" ;
+
+	html += `<!DOCTYPE html>
+	<html lang="en">
+	<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>ModusToolbox Asssitant</title>
+	</head>
+	<body>
+		<script>
+		function sendCommand(name: string)) {
+			const vscode = acquireVsCodeApi();
+			vscode.postMessage({ command: name }) ;
+		}
+		</script>
+	<h1>ModusToolbox Assistant</h1>
+	<h3>For ModusToolbox 3.0 from Infineon</h3>
+	<ul>
+	<li><a onclick="const vscode = acquireVsCodeApi() ; vscode.postMessage({ command: 'createNew'}) ;" href="#">Create A New Project</a></li>
+	<li><a onclick="const vscode = acquireVsCodeApi() ; vscode.postMessage({ command: 'importExisting'}) ;" href="#">Import An Existing Project</a></li>
+	<li><a onclick="const vscode = acquireVsCodeApi() ; vscode.postMessage({ command: 'showModusToolbox'}) ;" href="#">Show ModusToolbox View</a></li>
+	</ul>
+	</body>
+	</html>`;
+	panel.webview.html = html ;
+
+	panel.webview.onDidReceiveMessage( (message)=> {
+		if (message.command === "createNew") {
+			vscode.commands.executeCommand("mtbassist.mtbCreateProject") ;
+		}
+		else if (message.command === "importExisting") {
+			vscode.commands.executeCommand("mtbassist.mtbImportProject") ;
+		}
+		else if (message.command === "showModusToolbox") {
+			vscode.commands.executeCommand("mtbglobal.focus") ;
+		}
+	}) ;
 }
 
 // this method is called when your extension is deactivated
