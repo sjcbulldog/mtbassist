a2f90c7d0599dd047b383301ac9cd1f2341d8232
Butch Griffin <butchg@comcast.net>
2025-08-10 15:31:46 -0700

    added software installer

---
+++ content/src/app/software-installer/software-installer.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-software-installer',
+  template: `
+    <div class="software-installer-placeholder">
+      <h2>Software Installer</h2>
+      <p>This feature will allow you to install new software in the future.</p>
+    </div>
+  `,
+  styleUrls: ['./software-installer.component.scss']
+})
+export class SoftwareInstallerComponent {}
